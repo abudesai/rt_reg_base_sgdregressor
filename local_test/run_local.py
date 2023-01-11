34cd0e6613ca37f749cc888f0750717d3207142b
Abu Desai <abhyuday.desai@gmail.com>
2023-01-10 21:22:51 -0800

    empty hyperparameters file

--- local_test/run_local.py
+++ local_test/run_local.py
@@ -101,11 +101,6 @@ def copy_example_files(dataset_name):
         f"{local_datapath}/{dataset_name}/{dataset_name}_test.csv",
         os.path.join(test_data_path, f"{dataset_name}_test.csv"),
     )
-    # hyperparameters
-    shutil.copyfile(
-        "./examples/hyperparameters.json",
-        os.path.join(hyper_param_path, "hyperparameters.json"),
-    )
 
 
 def run_HPT(num_hpt_trials):
@@ -288,7 +283,7 @@ if __name__ == "__main__":
         "medical_costs",
         "white_wine",
     ]
-    datasets = ["white_wine"]
+    datasets = ["abalone"]
 
     for run_hpt in run_hpt_list:
         all_results = []
